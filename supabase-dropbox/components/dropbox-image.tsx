3e8c1e766839026822b34cbbecbf59b370f72d73
Minha Kang <118591632+m2na7@users.noreply.github.com>
2024-08-25 21:06:59 +0900

    파일 삭제 구현

--- supabase-dropbox/components/dropbox-image.tsx
+++ supabase-dropbox/components/dropbox-image.tsx
@@ -1,9 +1,21 @@
 "use client";
 
-import { IconButton } from "@material-tailwind/react";
+import { IconButton, Spinner } from "@material-tailwind/react";
+import { useMutation } from "@tanstack/react-query";
+import { deleteFile } from "actions/storageActions";
+import { queryClient } from "config/ReactQueryClientProvider";
 import { getImageUrl } from "utils/supabase/storage";
 
 export default function DropboxImage({ image }) {
+  const deleteFileMutation = useMutation({
+    mutationFn: deleteFile,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["images"],
+      });
+    },
+  });
+
   return (
     <div className="relative w-full flex flex-col gap-2 p-4 border border-gray-100 rounded-2xl shadow-md">
       <div>
@@ -12,10 +24,21 @@ export default function DropboxImage({ image }) {
           className="w-full aspect-square rounded-2xl"
         />
       </div>
+
       <div>{image.name}</div>
+
       <div className="absolute top-4 right-4">
-        <IconButton onClick={() => {}} color="red">
-          <i className="fas fa-trash" />
+        <IconButton
+          onClick={() => {
+            deleteFileMutation.mutate(image.name);
+          }}
+          color="red"
+        >
+          {deleteFileMutation.isPending ? (
+            <Spinner />
+          ) : (
+            <i className="fas fa-trash" />
+          )}
         </IconButton>
       </div>
     </div>
