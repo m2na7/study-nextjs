03454a4a106fcead8232439f8194f2a3fc053add
Minha Kang <118591632+m2na7@users.noreply.github.com>
2024-08-29 00:20:08 +0900

    Netflix clone ui구축 ~ 조회 기능 구현

---
+++ supabase-netflix/app/page.tsx
@@ -0,0 +1,10 @@
+import UI from "./ui";
+
+export const metadata = {
+  title: "TMDBFLIX",
+  description: "Nextflix clone",
+};
+
+export default function Home() {
+  return <UI />;
+}
---
+++ supabase-netflix/components/movie-card-list.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { useQuery } from "@tanstack/react-query";
+import MovieCard from "./movie-card";
+import { Spinner } from "@material-tailwind/react";
+import { searchMovies } from "actions/movieActions";
+import { useRecoilValue } from "recoil";
+import { searchState } from "utils/recoil/atoms";
+
+export default function MovieCardList() {
+  const search = useRecoilValue(searchState);
+  const getAllMoviesQuery = useQuery({
+    queryKey: ["movie", search],
+    queryFn: () => searchMovies(search),
+  });
+
+  return (
+    <div className="grid gap-1 md:grid-cols-4 grid-cols-3 w-full h-full">
+      {getAllMoviesQuery.isLoading && <Spinner />}
+      {getAllMoviesQuery.data &&
+        getAllMoviesQuery.data.map((movie) => (
+          <MovieCard key={movie.id} movie={movie} />
+        ))}
+    </div>
+  );
+}
